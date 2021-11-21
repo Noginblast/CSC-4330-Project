d53f4bd1e92e29877e48d37e8c82fd9bdab079b5
sarahbuckley323 <42855980+sarahbuckley323@users.noreply.github.com>
2021-11-21 15:27:35 -0600

    Had fun with user entries

--- lib/data/users.dart
+++ lib/data/users.dart
@@ -2,62 +2,31 @@ import 'package:ratr_dating/model/user.dart';
 
 final dummyUsers = [
   User(
-      name: 'Lauren Turner',
-      designation: 'Content Writer',
-      mutualFriends: 4,
-      bio:
-          'Psychology, science, and art are what helps me to learn the outside world and myself.',
-      age: 24,
-      imgUrl: 'assets/user1.jpg',
-      location: 'North London'),
-  User(
-    name: 'Lori Perez',
-    designation: 'UI Designer ',
-    mutualFriends: 8,
+    name: 'Princess Fiona',
+    designation: 'A fiery redhead',
     bio:
-        'Travelling, adventures, extreme sports are also an integral part of me, but I like watching and admiring extreme sports rather than doing it ?',
-    location: 'Leeds',
-    age: 26,
-    imgUrl: 'assets/user2.jpg',
-  ),
-  User(
-    name: 'Christine Wallace',
-    designation: 'News Reporter',
-    mutualFriends: 2,
-    bio:
-        'Psychology, science, and art are what helps me to learn the outside world and myself.',
-    location: 'Liverpool',
-    age: 23,
+        'I\'m a loaded pistol who likes pina coladas and getting caught in the rain',
+    ogre: '"no"',
+    location:
+        'A dragon-guarded castle surrounded by hot, boiling lava (but don\'t let that cool you off)',
     imgUrl: 'assets/user3.jpg',
   ),
   User(
-    name: 'Rachel Green',
-    designation: 'Architect',
-    mutualFriends: 8,
+    name: 'Snow White',
+    designation: 'A Cape wearing girl',
     bio:
-        'Psychology, science, and art are what helps me to learn the outside world and myself.',
-    location: 'Nottingham',
-    age: 22,
-    imgUrl: 'assets/user4.jpg',
+        'I live with seven other men, but I am NOT easy. Just kiss my dead, frozen lips and find out what a live wire I am!',
+    location: 'The land of fancy',
+    ogre: 'no',
+    imgUrl: 'assets/user2.jpg',
   ),
   User(
-    name: 'Emma',
-    designation: 'Software Developer',
-    mutualFriends: 3,
+    name: 'Cinderella',
+    designation: 'A mentally abused shut-in',
     bio:
-        'Psychology, science, and art are what helps me to learn the outside world and myself.',
-    location: 'Manchester',
-    age: 25,
-    imgUrl: 'assets/user5.jpg',
-  ),
-  User(
-    name: 'Kim Wexler',
-    designation: 'Accountant',
-    mutualFriends: 5,
-    bio:
-        'Psychology, science, and art are what helps me to learn the outside world and myself.',
-    location: 'Birmingham',
-    age: 30,
-    imgUrl: 'assets/user6.jpg',
+        'I like sushi and hottubing anytime! My hobbies include cooking and cleaning for my two evil sisters',
+    location: 'A kingdom far, far away',
+    ogre: 'no',
+    imgUrl: 'assets/user1.jpg',
   ),
 ];
--- lib/model/user.dart
+++ lib/model/user.dart
@@ -3,22 +3,20 @@ import 'package:flutter/cupertino.dart';
 class User {
   final String name;
   final String designation;
-  final int mutualFriends;
-  final int age;
   final String imgUrl;
   final String location;
   final String bio;
+  final String ogre;
   bool isLiked;
   bool isSwipedOff;
 
   User({
     @required this.designation,
-    @required this.mutualFriends,
     @required this.name,
-    @required this.age,
     @required this.imgUrl,
     @required this.location,
     @required this.bio,
+    @required this.ogre,
     this.isLiked = false,
     this.isSwipedOff = false,
   });
