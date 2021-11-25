86c955f03790a7c2c37ce9e96fd466058a61a8ca
Chris Chuang <67641657+cchuan3@users.noreply.github.com>
2021-11-25 03:31:33 -0600

    Delete android directory

--- android/app/src/main/kotlin/com/example/ratr_dating/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.ratr_dating
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- android/app/src/main/kotlin/com/example/tinder_ui_clone_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.tinder_ui_clone_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
