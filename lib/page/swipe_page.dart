432b29f02c1cdec423c44d90341440b5b065f2c0
Cade M Forbes <cadeforbes1@gmail.com>
2021-11-26 19:00:02 -0600

    Adjusted wording and font on out-of-matches message

--- lib/page/swipe_page.dart
+++ lib/page/swipe_page.dart
@@ -31,11 +31,14 @@ class _SwipePageState extends State<SwipePage> implements PreferredSizeWidget {
           padding: const EdgeInsets.all(8),
           child: Column(
             children: [
-              users.isEmpty
-                  ? const Text('No more users')
+              users.isEmpty? const Text(
+                'Uh Oh!!\nCome Back Tomorrow for More Matches!',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+              )
                   : Stack(children: users.map(buildUser).toList()),
               Expanded(child: Container()),
-              BottomButtonsWidget()
+              const BottomButtonsWidget()
             ],
           ),
         ),
